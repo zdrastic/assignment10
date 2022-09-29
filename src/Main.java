f74a4534636a850c80108eeb6dc2a7c64447849a
o.buravlov <zdrastic90@gmail.com>
2022-09-29 18:37:40 +0300

    development commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Home production work");
+        System.out.println("Development branch started");
     }
 }
