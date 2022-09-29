1ec1bb5c478b65fd8b339e5b825661b1b4d40ea7
o.buravlov <zdrastic90@gmail.com>
2022-09-29 16:27:04 +0300

    local production to master

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,7 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Try git again");
         System.out.println("message for github master");
+        System.out.println("message for production that will then appear in master");
+        System.out.println("local production to master");
     }
 }
