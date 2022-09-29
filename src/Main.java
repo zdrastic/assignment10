93aa471c6d24d48cb8b5a7fb33f8fccdacc80bcb
o.buravlov <zdrastic90@gmail.com>
2022-09-29 19:08:28 +0300

    development to production 1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Home production work");
-        System.out.println("Development branch started");
+        System.out.println("Home development work");
     }
 }
