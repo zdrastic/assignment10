6437a50e30c3258a7dc1016835f7e9787a2bb563
o.buravlov <zdrastic90@gmail.com>
2022-09-29 18:33:54 +0300

    home production test

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("Try git again");
-        System.out.println("message for github master");
-        System.out.println("message for production that will then appear in master");
-        System.out.println("local production to master");
+        System.out.println("Home production work");
     }
 }
