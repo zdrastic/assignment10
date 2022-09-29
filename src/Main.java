b8da46996b2577b757a816fcd50a647cf29ecee1
o.buravlov <zdrastic90@gmail.com>
2022-09-29 16:11:32 +0300

    Second commit

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Try git again");
+        System.out.println("message for github master");
     }
 }
