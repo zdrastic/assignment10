2cf243dab48ee850b46992d675d668069e358db8
o.buravlov <zdrastic90@gmail.com>
2022-09-29 16:27:04 +0300

    Production commit

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Try git again");
         System.out.println("message for github master");
+        System.out.println("message for production that will then appear in master");
     }
 }
