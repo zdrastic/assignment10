2be00c3dba77d7b9a6565b0347f04f1e17ef392a
o.buravlov <zdrastic90@gmail.com>
2022-09-30 11:20:48 +0300

    Addition added

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,77 @@
+import java.util.Scanner;
+
 public class Main {
+    public StringBuilder formula;
     public static void main(String[] args) {
-        System.out.println("work development work");
-        for (int i = 0; i < 5 ; i++) {
-            System.out.println(i);
+        Main m = new Main();
+
+        System.out.println("Input formula is " + args[0]);
+        m.formula = new StringBuilder(args[0]);
+        System.out.println(m.checkIfOnlyDigits());
+       /* while (!m.checkIfOnlyDigits()) {
+            m.doSingleOperation();
+        }*/
+        m.doSingleOperation();
+
+    }
+
+    private void doSingleOperation() {
+        raiseToPowerCheck();
+        multiplyCheck();
+        divisionCheck();
+        additionCheck();
+    }
+
+    private void additionCheck() {
+        int plusPosition;
+        int firstNumberStart = 0;
+        int secondNumberEnd = formula.length();
+        double result;
+        plusPosition = formula.indexOf("+");
+        if (plusPosition == (-1)) {
+            return;
+        } else {
+            for (int i = plusPosition-1; i > 0; i--) {
+                if (Character.isDigit(formula.charAt(i)) || formula.charAt(i) == ',') {
+                    firstNumberStart = i;
+                } else {
+                    break;
+                }
+            }
+            for (int i = plusPosition + 1; i < formula.length(); i++) {
+                if (Character.isDigit(formula.charAt(i)) || formula.charAt(i) == ',') {
+                    secondNumberEnd = i;
+                } else {
+                    break;
+                }
+            }
         }
-        System.out.println("friday working day started");
+        System.out.println("Addition numbers coordinates calculated correctly");
+        System.out.println(firstNumberStart);
+        System.out.println(secondNumberEnd);
+        result = Double.parseDouble(formula.substring(firstNumberStart, plusPosition)) +
+                Double.parseDouble(formula.substring(plusPosition + 1, secondNumberEnd + 1));
+        System.out.println(result);
+        System.out.println("Addition works well");
+    }
+
+    private void raiseToPowerCheck() {
+        
+    }
+
+    private void divisionCheck() {
+    }
+
+    private void multiplyCheck() {
+    }
+
+    public boolean checkIfOnlyDigits() {
+        for (int i = 0; i < formula.length(); i++) {
+            if (!Character.isDigit(formula.charAt(i))){
+                return false;
+            }
+        }
+        return true;
     }
 
 }
